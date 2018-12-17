1b8f2d1cfe6222d0df7363da3789bf0cb2472f92
bredele <olivier.wietrich@gmail.com>
2018-12-17 11:07:14 -0700

    Extract even and off characters

--- test/even-odd.js
+++ test/even-odd.js
@@ -6,10 +6,18 @@ const test = require('tape')
 const extract = require('..')
 
 
-test('convert string into bits #1', assert => {
+test('extract even and off characters #1', assert => {
   assert.plan(1)
   assert.deepEqual(
     extract('11010000110001100100'),
     ['1000100100', '1100101010']
+  )
+})
+
+test('extract even and off characters #12', assert => {
+  assert.plan(1)
+  assert.deepEqual(
+    extract('helloworld'),
+    ['hlool', 'elwrd']
   )
 })
